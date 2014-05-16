449f7efa948f40cf770faeccb827e6a1683a1982
Brenton Simpson <git@appsforartists.com>
2014-05-16 16:22:06 -0700

    Preventing others from responding to ctrl-;

--- source/common/keystroke_manager.js
+++ source/common/keystroke_manager.js
@@ -1,11 +1,12 @@
 function onKeyDown(event) {
-  switch (event.keyCode) {
-    case KeyCode.SEMICOLON:
-      if (event.ctrlKey)
-        gssVisualizer.enabled = !gssVisualizer.enabled;
-
-      break;
-  }
+    switch (event.keyCode) {
+        case KeyCode.SEMICOLON:
+            if (event.ctrlKey) {
+                gssVisualizer.enabled = !gssVisualizer.enabled;
+                return false;
+            }
+            break;
+    }
 }
 
 window.addEventListener('keydown', onKeyDown);
